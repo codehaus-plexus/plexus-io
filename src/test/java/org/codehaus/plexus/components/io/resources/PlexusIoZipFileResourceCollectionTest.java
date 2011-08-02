5a03ea9a8916ea79847fde204459c6244b29f5ec
Kristian Rosenvold <kristian@rosenvold.com>
2011-08-02 19:04:38 +0200

    [PLXCOMP-170] Testcase
    Patch and testcase by Robert Scholte, applied unmodified

---
+++ src/test/java/org/codehaus/plexus/components/io/resources/PlexusIoZipFileResourceCollectionTest.java
@@ -0,0 +1,22 @@
+package org.codehaus.plexus.components.io.resources;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Iterator;
+
+import org.codehaus.plexus.PlexusTestCase;
+
+public class PlexusIoZipFileResourceCollectionTest
+    extends PlexusTestCase
+{
+
+    public void testNamelessRootFolder() throws Exception {
+        PlexusIoZipFileResourceCollection resourceCollection = new PlexusIoZipFileResourceCollection();
+        resourceCollection.setFile( getTestFile( "src/test/jars/namelessrootfolder.jar" ) );
+        Iterator iterator = resourceCollection.getEntries();
+        PlexusIoURLResource entry = (PlexusIoURLResource) iterator.next();
+        assertEquals( "/dummy.txt", entry.getName() );
+        BufferedReader d = new BufferedReader(new InputStreamReader(entry.getContents()));
+        assertEquals( "dummy content", d.readLine() );
+    }
+}
