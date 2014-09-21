47406b48ad600b83cd3c3121471a1f7e375a78eb
Kristian Rosenvold <krosenvold@apache.org>
2014-09-21 21:47:40 +0200

    Added support for java7 chmod that can do RWX for all three roles

--- src/main/java/org/codehaus/plexus/components/io/attributes/Java7AttributeUtils.java
+++ src/main/java/org/codehaus/plexus/components/io/attributes/Java7AttributeUtils.java
@@ -3,7 +3,11 @@ package org.codehaus.plexus.components.io.attributes;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.PosixFilePermission;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author Kristian Rosenvold
@@ -25,5 +29,28 @@ public class Java7AttributeUtils
             throw new RuntimeException( e );
         }
 
+    }
+
+    public static void chmod(File file, int mode)
+        throws IOException
+    {
+        Files.setPosixFilePermissions( file.toPath(), getPermissions( mode ));
+    }
+
+    static Set<PosixFilePermission> getPermissions(int mode){
+        Set<PosixFilePermission> perms = new HashSet<PosixFilePermission>();
+        //add owners permission
+        if ((mode & 0400) > 0) perms.add(PosixFilePermission.OWNER_READ);
+        if ((mode & 0200) > 0) perms.add(PosixFilePermission.OWNER_WRITE);
+        if ((mode & 0100) > 0) perms.add(PosixFilePermission.OWNER_EXECUTE);
+        //add group permissions
+        if ((mode & 0040) > 0) perms.add(PosixFilePermission.GROUP_READ);
+        if ((mode & 0020) > 0) perms.add(PosixFilePermission.GROUP_WRITE);
+        if ((mode & 0010) > 0) perms.add(PosixFilePermission.GROUP_EXECUTE);
+        //add others permissions
+        if ((mode & 0004) > 0) perms.add(PosixFilePermission.OTHERS_READ);
+        if ((mode & 0002) > 0) perms.add(PosixFilePermission.OTHERS_WRITE);
+        if ((mode & 0001) > 0) perms.add(PosixFilePermission.OTHERS_EXECUTE);
+        return perms;
     }
 }
---
+++ src/test/java/org/codehaus/plexus/components/io/attributes/Java7AttributeUtilsTest.java
@@ -0,0 +1,67 @@
+package org.codehaus.plexus.components.io.attributes;
+
+import junit.framework.TestCase;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.attribute.PosixFilePermission;
+import java.util.HashMap;
+import java.util.Set;
+
+/**
+ * @author Kristian Rosenvold
+ */
+public class Java7AttributeUtilsTest
+    extends TestCase
+{
+    public void testMiscPatterns()
+        throws Exception
+    {
+        final Set<PosixFilePermission> permissions = Java7AttributeUtils.getPermissions( 0124 );
+        assertTrue( permissions.contains( PosixFilePermission.OWNER_EXECUTE ) );
+        assertTrue(permissions.contains( PosixFilePermission.GROUP_WRITE ));
+        assertTrue(permissions.contains( PosixFilePermission.OTHERS_READ ));
+    }
+
+    public void testMorePatterns()
+        throws Exception
+    {
+        final Set<PosixFilePermission> permissions = Java7AttributeUtils.getPermissions( 0241 );
+        assertTrue(permissions.contains( PosixFilePermission.OWNER_WRITE ));
+        assertTrue(permissions.contains( PosixFilePermission.GROUP_READ ));
+        assertTrue(permissions.contains( PosixFilePermission.OTHERS_EXECUTE ));
+    }
+
+    public void testEvenMorePatterns()
+        throws Exception
+    {
+        final Set<PosixFilePermission> permissions = Java7AttributeUtils.getPermissions( 0412 );
+        assertTrue(permissions.contains( PosixFilePermission.OWNER_READ ));
+        assertTrue(permissions.contains( PosixFilePermission.GROUP_EXECUTE ));
+        assertTrue(permissions.contains( PosixFilePermission.OTHERS_WRITE ));
+    }
+
+    public void test777()
+        throws Exception
+    {
+        final Set<PosixFilePermission> permissions = Java7AttributeUtils.getPermissions( 0777 );
+        assertTrue( permissions.size() == 9);
+    }
+      
+    public void testChmodBackAndForth()
+        throws IOException
+    {
+        final File bxx = File.createTempFile( "bxx", "ff" );
+        System.out.println( "bxx = " + bxx );
+        Java7AttributeUtils.chmod( bxx, 0422 );
+        Java7FileAttributes firstAttrs = new Java7FileAttributes( bxx, new HashMap<Integer, String>(  ), new HashMap<Integer, String>(  ) );
+        assertTrue( firstAttrs.isOwnerReadable() );
+        assertFalse( firstAttrs.isOwnerWritable() );
+        assertFalse( firstAttrs.isOwnerExecutable() );
+        Java7AttributeUtils.chmod( bxx, 0777 );
+        Java7FileAttributes secondAttrs = new Java7FileAttributes( bxx, new HashMap<Integer, String>(  ), new HashMap<Integer, String>(  ) );
+        assertTrue( secondAttrs.isOwnerReadable() );
+        assertTrue( secondAttrs.isOwnerWritable() );
+        assertTrue( secondAttrs.isOwnerExecutable() );
+    }
+}
