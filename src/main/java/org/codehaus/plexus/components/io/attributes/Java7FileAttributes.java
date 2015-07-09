393efb9bf9aef29ebc6479d5c8f493d308476e9c
Kristian Rosenvold <krosenvold@apache.org>
2015-07-09 21:11:42 +0200

    Extracted variable

--- src/main/java/org/codehaus/plexus/components/io/attributes/Java7FileAttributes.java
+++ src/main/java/org/codehaus/plexus/components/io/attributes/Java7FileAttributes.java
@@ -20,6 +20,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.LinkOption;
+import java.nio.file.Path;
 import java.nio.file.attribute.FileOwnerAttributeView;
 import java.nio.file.attribute.PosixFilePermission;
 import java.security.Principal;
@@ -36,8 +37,7 @@ import javax.annotation.Nullable;
  * Immutable
  */
 public class Java7FileAttributes
-    implements PlexusIoResourceAttributes
-{
+        implements PlexusIoResourceAttributes {
     @Nullable
     private final Integer groupId;
 
@@ -55,223 +55,189 @@ public class Java7FileAttributes
 
     private final Set<PosixFilePermission> permissions;
 
-    public Java7FileAttributes( @Nonnull File file,  @Nonnull Map<Integer, String> userCache,
-                                @Nonnull Map<Integer, String> groupCache )
-        throws IOException
-    {
+    public Java7FileAttributes(@Nonnull File file, @Nonnull Map<Integer, String> userCache,
+                               @Nonnull Map<Integer, String> groupCache)
+            throws IOException {
 
 
-        if (Java7AttributeUtils.isUnix(file.toPath()))
-        {
-            Map<String, Object> attrs = Files.readAttributes(file.toPath(), "unix:*", LinkOption.NOFOLLOW_LINKS);
+        Path path = file.toPath();
+        if (Java7AttributeUtils.isUnix(path)) {
+            Map<String, Object> attrs = Files.readAttributes(path, "unix:*", LinkOption.NOFOLLOW_LINKS);
             this.permissions = (Set<PosixFilePermission>) attrs.get("permissions");
 
             groupId = (Integer) attrs.get("gid");
 
-            String groupName = groupCache.get( groupId );
-            if ( groupName != null )
-            {
+            String groupName = groupCache.get(groupId);
+            if (groupName != null) {
                 this.groupName = groupName;
-            }
-            else
-            {
+            } else {
                 this.groupName = ((Principal) attrs.get("group")).getName();
-                groupCache.put( groupId, this.groupName );
+                groupCache.put(groupId, this.groupName);
             }
             userId = (Integer) attrs.get("uid");
-            String userName = userCache.get( userId );
-            if ( userName != null )
-            {
+            String userName = userCache.get(userId);
+            if (userName != null) {
                 this.userName = userName;
-            }
-            else
-            {
+            } else {
                 this.userName = ((Principal) attrs.get("owner")).getName();
-                userCache.put( userId, this.userName );
+                userCache.put(userId, this.userName);
             }
-            octalMode = (Integer)attrs.get("mode") & 0xfff; // Mask off top bits for compatibilty. Maybe check if we can skip this
+            octalMode = (Integer) attrs.get("mode") & 0xfff; // Mask off top bits for compatibilty. Maybe check if we can skip this
             symbolicLink = (Boolean) attrs.get("isSymbolicLink");
         } else {
-            FileOwnerAttributeView fa = Java7AttributeUtils.getFileOwnershipInfo( file );
+            FileOwnerAttributeView fa = Java7AttributeUtils.getFileOwnershipInfo(file);
             this.userName = fa.getOwner().getName();
             userId = null;
             this.groupName = null;
             this.groupId = null;
             octalMode = -1;
             permissions = Collections.emptySet();
-            symbolicLink = Files.isSymbolicLink(file.toPath());
+            symbolicLink = Files.isSymbolicLink(path);
         }
 
     }
 
-    public static  @Nonnull PlexusIoResourceAttributes uncached(  @Nonnull File file )
-        throws IOException
-    {
-        return new Java7FileAttributes( file, new HashMap<Integer, String>(), new HashMap<Integer, String>() );
+    public static
+    @Nonnull
+    PlexusIoResourceAttributes uncached(@Nonnull File file)
+            throws IOException {
+        return new Java7FileAttributes(file, new HashMap<Integer, String>(), new HashMap<Integer, String>());
     }
 
 
-    @Nullable public Integer getGroupId()
-    {
+    @Nullable
+    public Integer getGroupId() {
 
         return groupId;
     }
 
-    public boolean hasGroupId()
-    {
+    public boolean hasGroupId() {
         return false;
     }
 
-    public boolean hasUserId()
-    {
+    public boolean hasUserId() {
         return false;
     }
 
-    @Nullable public String getGroupName()
-    {
+    @Nullable
+    public String getGroupName() {
         return groupName;
     }
 
-    public Integer getUserId()
-    {
+    public Integer getUserId() {
         return userId;
     }
 
-    public String getUserName()
-    {
+    public String getUserName() {
         return userName;
     }
 
-    public boolean isGroupExecutable()
-    {
-        return containsPermission( PosixFilePermission.GROUP_EXECUTE );
+    public boolean isGroupExecutable() {
+        return containsPermission(PosixFilePermission.GROUP_EXECUTE);
     }
 
-    private boolean containsPermission( PosixFilePermission groupExecute )
-    {
-        return permissions.contains( groupExecute );
+    private boolean containsPermission(PosixFilePermission groupExecute) {
+        return permissions.contains(groupExecute);
     }
 
-    public boolean isGroupReadable()
-    {
-        return containsPermission( PosixFilePermission.GROUP_READ );
+    public boolean isGroupReadable() {
+        return containsPermission(PosixFilePermission.GROUP_READ);
     }
 
-    public boolean isGroupWritable()
-    {
-        return containsPermission( PosixFilePermission.GROUP_WRITE );
+    public boolean isGroupWritable() {
+        return containsPermission(PosixFilePermission.GROUP_WRITE);
     }
 
-    public boolean isOwnerExecutable()
-    {
-        return containsPermission( PosixFilePermission.OWNER_EXECUTE );
+    public boolean isOwnerExecutable() {
+        return containsPermission(PosixFilePermission.OWNER_EXECUTE);
     }
 
-    public boolean isOwnerReadable()
-    {
-        return containsPermission( PosixFilePermission.OWNER_READ );
+    public boolean isOwnerReadable() {
+        return containsPermission(PosixFilePermission.OWNER_READ);
     }
 
-    public boolean isOwnerWritable()
-    {
-        return containsPermission( PosixFilePermission.OWNER_WRITE );
+    public boolean isOwnerWritable() {
+        return containsPermission(PosixFilePermission.OWNER_WRITE);
     }
 
-    public boolean isWorldExecutable()
-    {
-        return containsPermission( PosixFilePermission.OTHERS_EXECUTE );
+    public boolean isWorldExecutable() {
+        return containsPermission(PosixFilePermission.OTHERS_EXECUTE);
 
     }
 
-    public boolean isWorldReadable()
-    {
-        return containsPermission( PosixFilePermission.OTHERS_READ );
+    public boolean isWorldReadable() {
+        return containsPermission(PosixFilePermission.OTHERS_READ);
     }
 
-    public boolean isWorldWritable()
-    {
-        return containsPermission( PosixFilePermission.OTHERS_WRITE );
+    public boolean isWorldWritable() {
+        return containsPermission(PosixFilePermission.OTHERS_WRITE);
     }
 
-    public String toString()
-    {
+    public String toString() {
         StringBuilder sb = new StringBuilder();
-        sb.append( "\nFile Attributes:\n------------------------------\nuser: " );
-        sb.append( userName == null ? "" : userName );
-        sb.append( "\ngroup: " );
-        sb.append( groupName == null ? "" : groupName );
-        sb.append( "\nuid: " );
-        sb.append( hasUserId() ? Integer.toString( userId ) : "" );
-        sb.append( "\ngid: " );
-        sb.append( hasGroupId() ? Integer.toString( groupId ) : "" );
+        sb.append("\nFile Attributes:\n------------------------------\nuser: ");
+        sb.append(userName == null ? "" : userName);
+        sb.append("\ngroup: ");
+        sb.append(groupName == null ? "" : groupName);
+        sb.append("\nuid: ");
+        sb.append(hasUserId() ? Integer.toString(userId) : "");
+        sb.append("\ngid: ");
+        sb.append(hasGroupId() ? Integer.toString(groupId) : "");
 
         return sb.toString();
     }
 
-    public int getOctalMode()
-    {
+    public int getOctalMode() {
         return octalMode;
     }
 
-    public int calculatePosixOctalMode()
-    {
+    public int calculatePosixOctalMode() {
         int result = 0;
 
-        if ( isOwnerReadable() )
-        {
+        if (isOwnerReadable()) {
             result |= AttributeConstants.OCTAL_OWNER_READ;
         }
 
-        if ( isOwnerWritable() )
-        {
+        if (isOwnerWritable()) {
             result |= AttributeConstants.OCTAL_OWNER_WRITE;
         }
 
-        if ( isOwnerExecutable() )
-        {
+        if (isOwnerExecutable()) {
             result |= AttributeConstants.OCTAL_OWNER_EXECUTE;
         }
 
-        if ( isGroupReadable() )
-        {
+        if (isGroupReadable()) {
             result |= AttributeConstants.OCTAL_GROUP_READ;
         }
 
-        if ( isGroupWritable() )
-        {
+        if (isGroupWritable()) {
             result |= AttributeConstants.OCTAL_GROUP_WRITE;
         }
 
-        if ( isGroupExecutable() )
-        {
+        if (isGroupExecutable()) {
             result |= AttributeConstants.OCTAL_GROUP_EXECUTE;
         }
 
-        if ( isWorldReadable() )
-        {
+        if (isWorldReadable()) {
             result |= AttributeConstants.OCTAL_WORLD_READ;
         }
 
-        if ( isWorldWritable() )
-        {
+        if (isWorldWritable()) {
             result |= AttributeConstants.OCTAL_WORLD_WRITE;
         }
 
-        if ( isWorldExecutable() )
-        {
+        if (isWorldExecutable()) {
             result |= AttributeConstants.OCTAL_WORLD_EXECUTE;
         }
 
         return result;
     }
 
-    public String getOctalModeString()
-    {
-        return Integer.toString( getOctalMode(), 8 );
+    public String getOctalModeString() {
+        return Integer.toString(getOctalMode(), 8);
     }
 
-    public boolean isSymbolicLink()
-    {
+    public boolean isSymbolicLink() {
         return symbolicLink;
     }
 }
